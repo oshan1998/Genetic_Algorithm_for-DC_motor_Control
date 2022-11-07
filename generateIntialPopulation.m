e3556456221912d5e9871debc31631927e51cc98
Oshan <oshanrajapaksha98@gmail.com>
2022-11-07 15:05:37 +0530

    changed initial population dimension to 3x8x100

--- generateIntialPopulation.m
+++ generateIntialPopulation.m
@@ -1,20 +1,19 @@
 % function is used to generate an initial population with random kp,ki,kd
 % values
 
-function pop = generateIntialPopulation(populationSize, sys)
-    max_gain = 150;
+function[ pop,cost] = generateIntialPopulation(populationSize,sys)
+    %max_gain = 150;
+    cost = zeors(1,100);
+    nDigit = 8;
+    pop = randi([0 1],1,3,nDigit,populationSize); 
     
-    pop = zeros(4, populationSize);
+    
     
     for i=1:populationSize
-        pop(1:3, i) = rand(3,1)*max_gain;  
-    end
-    
-    for i=1:populationSize
-        kp = pop(1,i);
-        ki = pop(2,i);
-        kd = pop(3,i);
+        kp = pop(1,:,i);
+        ki = pop(2,:,i);
+        kd = pop(3,:,i);
         
-        pop(4,i) = calculateFitness(sys,kp,ki,kd);
+        cost(1,i) = calculateFitness(sys,kp,ki,kd);
     end
 end
