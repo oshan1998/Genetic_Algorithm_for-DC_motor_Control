257772d961f22321c37e39ef9e0a89bfcb2ea821
Oshan <oshanrajapaksha98@gmail.com>
2022-11-07 10:35:57 +0530

    select parent function modified in optimum way of doing roulette wheel selection.

--- selectParents.m
+++ selectParents.m
@@ -1,34 +1,45 @@
 % parents are selected based on Roulette Wheel Selection
-function [parent1, parent2] = selectParents(population)
+function [parent1,parent2] = selectParents(population)
     fitnesses = population(4,:);
-    
-    fitnesses = fitnesses/sum(fitnesses);
-    
-    parent_1_probability = rand();
-    parent_2_probability = rand();
-    
-    cumalative_sum = 0;
-    
-    num_selected_parents = 0;
-    
-    for i=1:length(fitnesses)
-        
-        if (num_selected_parents == 2)
-            break
-        end
-        
-        if (parent_1_probability >= cumalative_sum && parent_1_probability <= cumalative_sum + fitnesses(1,i))
-            parent1 = population(:,i);
-            num_selected_parents = num_selected_parents + 1;
-        end
-        
-        if (parent_2_probability >= cumalative_sum && parent_2_probability <= cumalative_sum + fitnesses(1,i))
-            parent2 = population(:,i);
-            num_selected_parents = num_selected_parents + 1;
-        end
-        
-        cumalative_sum = cumalative_sum +  fitnesses(1,i);
+    % mapping fitness values to probabilities
+    beta = 1;
+    avgFitness = mean(fitnesses);
+    if avgFitness ~=0
+        fitnesses = fitnesses/avgFitness;
     end
+    probs = exp(-beta*fitnesses);
+    cumsumProbs = cumsum(probs);
+    
+    
+%     fitnesses = fitnesses/sum(fitnesses);
+    
+     parent_1_probability = rand();
+     parent_2_probability = rand();
+     parent1 = find(parent_1_probability<=cumsumProbs,1,'first');
+     parent2 = find(parent_2_probability<=cumsumProbs,1,'first');
+%     
+%     cumalative_sum = 0;
+%     
+%     num_selected_parents = 0;
+%     
+%     for i=1:length(fitnesses)
+%         
+%         if (num_selected_parents == 2)
+%             break
+%         end
+%         
+%         if (parent_1_probability >= cumalative_sum && parent_1_probability <= cumalative_sum + fitnesses(1,i))
+%             parent1 = population(:,i);
+%             num_selected_parents = num_selected_parents + 1;
+%         end
+%         
+%         if (parent_2_probability >= cumalative_sum && parent_2_probability <= cumalative_sum + fitnesses(1,i))
+%             parent2 = population(:,i);
+%             num_selected_parents = num_selected_parents + 1;
+%         end
+%         
+%         cumalative_sum = cumalative_sum +  fitnesses(1,i);
+%     end
     
     
 end
